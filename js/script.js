642556fa8c6e14971b68af76a20505040f3d1cc5
gui100O <guilhermefasousa@gmail.com>
2026-08-23 13:48:04 -0300

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+
+sections.forEach(section => {
+    observer.observe(section);
+});
